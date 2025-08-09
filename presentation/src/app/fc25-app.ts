b62728626a8a78e7e6328a5a0505e3573d01f55a
Artem Shinkaruk <dlartagnan@yandex-team.ru>
2025-08-10 00:54:40 +0200

    feat(presentation): added routing

--- presentation/src/app/fc25-app.ts
+++ presentation/src/app/fc25-app.ts
@@ -1,17 +1,26 @@
 import { LitElement, html, css, unsafeCSS } from 'lit';
 import { customElement } from 'lit/decorators.js';
-
-import theme from '../styles/theme-vscode.css?inline';
+import { Router } from '@lit-labs/router';
 
 import 'playground-elements';
 import '../components/drawing-overlay.js';
 
+import type { ProjectManifest } from '../config/interface.ts';
 import type { DrawingElement } from '../components/drawing-overlay.js';
 
+// Examples
 import shadowDomStylesEncapsulation from '../config/shadow-dom-styles-encapsulation.ts';
 import shadowDomStyling, {
   drawHistory as shadowDomStylingDrawHistory,
 } from '../config/shadow-dom-styling.ts';
+
+// Styles
+import theme from '../styles/theme-vscode.css?inline';
+
+const examples: Record<string, ProjectManifest> = {
+  'shadow-dom-encapsulation': shadowDomStylesEncapsulation,
+  'shadow-dom-styling': shadowDomStyling,
+};
 
 @customElement('fc25-app')
 export class FC25App extends LitElement {
@@ -24,11 +33,28 @@ export class FC25App extends LitElement {
         height: 100%;
       }
 
+      ul {
+        list-style: square;
+        color: var(--yellow);
+        font-size: 2rem;
+      }
+
+      a {
+        color: var(--yellow);
+        text-decoration: none;
+      }
+
+      a:hover {
+        text-decoration: underline;
+      }
+
       playground-ide {
+        box-sizing: border-box;
         --playground-preview-width: 40%;
         --playground-code-font-family: 'JetBrains Mono', monospace;
         --playground-code-font-size: 2rem;
         --playground-code-line-padding: 0 1rem;
+        border-bottom: var(--playground-border);
         width: 100%;
         height: 100%;
       }
@@ -40,12 +66,32 @@ export class FC25App extends LitElement {
     unsafeCSS(theme),
   ];
 
+  private routes = new Router(this, [
+    { path: '/', render: () => this.renderMenu() },
+    {
+      path: '/:example',
+      render: ({ example }) => this.renderExample(example!),
+    },
+  ]);
+
   override render() {
+    this.routes.routes;
+    return this.routes.outlet();
+  }
+
+  renderMenu() {
+    return html`<ul>
+      <li><a href="/shadow-dom-encapsulation">Инкапсуляция Shadow DOM</a></li>
+      <li><a href="/shadow-dom-styling">Стилизация Shadow DOM</a></li>
+    </ul>`;
+  }
+
+  renderExample(example: string) {
     return html`
       <playground-ide
         editable-file-system
         resizable
-        .config=${shadowDomStyling}
+        .config=${examples[example]}
       >
       </playground-ide>
 
